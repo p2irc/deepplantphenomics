452ad60def56393a0b23cddd29d37f8f0e87dcfc
Jordan Ubbens <jubbens@gmail.com>
2017-11-29 12:16:04 -0600

    added an example of how to apply the segmentation masks

--- examples/semantic_segmentation_test.py
+++ examples/semantic_segmentation_test.py
@@ -3,7 +3,7 @@
 #
 
 import deepplantphenomics as dpp
-import numpy
+import numpy as np
 # Required to write the results as images
 from PIL import Image
 import os
@@ -20,8 +20,22 @@ print('Performing segmentation...')
 y = dpp.tools.segment_vegetation(images)
 
 for i, img in enumerate(y):
+    # Get original image dimensions
+    org_filename = images[i]
+    org_img = Image.open(org_filename)
+    org_width, org_height = org_img.size
+    org_array = np.array(org_img)
+
+    # Resize mask
+    mask_img = Image.fromarray((img * 255).astype(np.uint8))
+    mask_array = np.array(mask_img.resize((org_width, org_height))) / 255
+
+    # Apply mask
+    img_seg = np.array([org_array[:,:,0] * mask_array, org_array[:,:,1] * mask_array, org_array[:,:,2] * mask_array]).transpose()
+
+    # Write output file
     filename = os.path.join(output_dir, os.path.basename(images[i]))
-    result = Image.fromarray((img * 255).astype(numpy.uint8))
+    result = Image.fromarray(img_seg.astype(np.uint8))
     result.save(filename)
 
 print('Done')
