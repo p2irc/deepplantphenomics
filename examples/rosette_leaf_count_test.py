9233f15d56b83990c2d831addd665cf8fed2b377
nhiggs <nicoreekohiggs@gmail.com>
2018-05-16 15:55:49 -0600

    updated print to pyth3 and fixed data path

--- examples/rosette_leaf_count_test.py
+++ examples/rosette_leaf_count_test.py
@@ -6,8 +6,7 @@
 import deepplantphenomics as dpp
 import os
 
-dir = '/home/nico/Plant_Phenotyping_Datasets/Plant/Ara2013-Canon'
-#dir = './data/Ara2013-Canon'
+dir = './data/Ara2013-Canon'
 
 images = [os.path.join(dir, name) for name in os.listdir(dir) if
           os.path.isfile(os.path.join(dir, name)) & name.endswith('_rgb.png')]
@@ -20,6 +19,6 @@ print('Performing leaf estimation...')
 y = dpp.tools.predict_rosette_leaf_count(images)
 
 for k,v in zip(images, y):
-    print '%s: %d' % (os.path.basename(k), v)
+    print('%s: %d' % (os.path.basename(k), v))
 
 print('Done')
